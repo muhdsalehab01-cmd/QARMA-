309b682ea067ea1c8ca7248c52aa859711d0524a
muhdsalehab01-cmd <muhdsaleh.ab01@gmail.com>
2026-05-12 22:12:34 +0100

    Update qarma.py

--- qarma.py
+++ qarma.py
@@ -3,7 +3,7 @@
 #
 # Marubuci / Author: Muhammad Saleh Abdulhamid
 # Wuri / Location: Nigeria
-# Sigar / Version: 4.0.0
+# Sigar / Version: 1.0
 # Farawa / Started: 2026
 # Harshe / Language: Python
 # Yanayi / Status: Active Development
